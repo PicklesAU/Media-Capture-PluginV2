fa8f4958169a1ced06e1b3db18cc1b55d87411ca
PicklesAU <34080434+PicklesAU@users.noreply.github.com>
2020-11-13 11:40:15 +0700

    Update Capture.java

--- src/android/Capture.java
+++ src/android/Capture.java
@@ -435,9 +435,9 @@ public class Capture extends CordovaPlugin {
         if (intent != null){
             // Get the uri of the video clip
             data = intent.getData();
-		
+	    File fp = webView.getResourceApi().mapUriToFile(data)
             File movie = new File(getTempDirectoryPath(), "DIY.mp4");
-	    copyFile(movie, data);
+	    copyFile(movie, fp);
             data = Uri.fromFile(movie);
         }
 
